f0b4cb832b5ae25543e601f84d104dd6c8fdfa93
Samir Hawaldar <samir_hawaldar@yahoo.com>
2023-04-24 15:51:29 +0530

    Change: singleInput.py
    Now the script will also demonstrate the how to receive the cli input as
    a List, Tuple, Set.

--- cliInput/singleInput.py
+++ cliInput/singleInput.py
@@ -1,14 +1,45 @@
 #Taking the single input from command line input.
 
+def Display(loc_a):
+    print("You Enterned: ", loc_a, "\n")
+
 #Intefer
 int_a = int(input('Enter an integer value: '))
+Display(int_a)
+
 
 #Decimal/Floating
 float_a = float(input('Enter a floating/decimal value: '))
+Display(float_a)
+
 
 #Character/String
 str_a = input('Enter Character or String: ')
+Display(str_a)
+
+
+#List with same data type.
+list_int = list(map(int, input("Enter Interger list: ").split()))
+print('Your list: ', list_int, "\n")
+
+
+#List with Dissimilar Data
+#Note: Data will be stored as a string in list.
+list_dis = list(input("Enter any data: ").split())
+print('You Dissimilar Data List: ',list_dis, "\n")
+
+
+#Set with Dissimilar Data
+#Note: Set holds unique values.
+set_dis = set(input("Enter any data: ").split())
+print('You Set Data: ',set_dis, "\n Duplicate entries will not be considered. Set holds uniques entries. Sets cannot be indexed.", '\n')
+
+
+#Tuple
+tup_a = tuple(input('Enter data for Tuple: ').split())
+print('You Tuple Data: ',tup_a, "\nEnterned data cannot be changed. Tuple is mutable.", '\n')
+
 
 
 if __name__ == "__main__":
-    print('Interger: {0}\nDecimal: {1}\nCharacter/String: {2}'.format(int_a, float_a, str_a))
+    pass
